196ac993c6016f64bc36f187ad2434ca66b38bfe
leerichardson <leerichardson2013@gmail.com>
2016-08-25 10:29:27 -0400

    Pushing marginal data for South Dakota

---
+++ data-raw/get_sd_tables.R
@@ -0,0 +1,37 @@
+# SKG
+# April 13, 2016
+# Getting ACS data with the acs package
+# Updated LFR, 8/17/2016
+
+# Use spew and acs package to ACS summary tables  
+library(acs)
+devtools::load_all()
+
+# Loop through South Dakota counties downloading the 4 tables 
+table_numbers <- c("B19001", "B11016", "B25007", "B25006")
+table_ids <- c("HHInc", "HHSize", "HHHAge", "HHHRace")
+counter <- 0
+
+for (ii in 1:length(table_numbers)) {
+  for (row in 1:nrow(fips.county)) {
+    
+    counter <- counter + 1
+    if (counter %% 100 == 0) {
+      print(counter)
+      Sys.sleep(5)
+    }
+
+    st <- fips.county$State[row]
+    co <- fips.county$County.ANSI[row]
+    
+    # Only download the South Dakota datasets 
+    if (st != "SD") {
+      next
+    }
+    
+    geo_list <- list(state = st, county = co,  tract = "*")
+    acs_fetch_list <- list(endyear = 2014, span = 5, table.number = table_numbers[ii])
+    test <- fetchAndWriteACS(geo_list, acs_fetch_list, outPath = "data-raw/46/marginals", 
+                             tabID = table_ids[ii])
+  }
+}
--- data-raw/read_us.R
+++ data-raw/read_us.R
@@ -6,65 +6,74 @@
 
 
 # Test the individual read_data functions ----------------------
-
 sd_poptable <- read_pop_table("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
                                                                           pums = "pums", 
                                                                           schools = "schools", 
                                                                           lookup = "tables", 
                                                                           shapefiles = "tiger", 
-                                                                          workplaces = "workplaces")) 
+                                                                          workplaces = "workplaces",
+                                                                          marginals = "marginals")) 
 sd_poptable <- standardize_pop_table(sd_poptable, data_group = "US")
 
 
-
+# PUMS 
 sd_pums <- read_pums("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
-                                                                               pums = "pums", 
-                                                                               schools = "schools", 
-                                                                               lookup = "tables", 
-                                                                               shapefiles = "tiger", 
-                                                                               workplaces = "workplaces")) 
-sd_pums <- standardize_pums(sd_pums, data_group = "US")
-
-
-
-
-
-
-sd_shape <- read_shapefiles("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
                                                                       pums = "pums", 
                                                                       schools = "schools", 
                                                                       lookup = "tables", 
                                                                       shapefiles = "tiger", 
-                                                                      workplaces = "workplaces")) 
+                                                                      workplaces = "workplaces",
+                                                                      marginals = "marginals")) 
+sd_pums <- standardize_pums(sd_pums, data_group = "US")
 
+
+# Shapefiles ------
+sd_shape <- read_shapefiles("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
+                                                                              pums = "pums", 
+                                                                              schools = "schools", 
+                                                                              lookup = "tables", 
+                                                                              shapefiles = "tiger", 
+                                                                              workplaces = "workplaces",
+                                                                              marginals = "marginals")) 
 sd_shape <- standardize_shapefiles(sd_shape, data_group = "US")
 
-
-
-  
+# Lookup tables -----  
 sd_lookup <- read_lookup("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
                                                              pums = "pums", 
                                                              schools = "schools", 
                                                              lookup = "tables", 
                                                              shapefiles = "tiger", 
-                                                             workplaces = "workplaces"))
+                                                             workplaces = "workplaces",
+                                                             marginals = "marginals"))
 sd_lookup <- standardize_lookup(sd_lookup, data_group = "US")
 
 
+# Schools 
 sd_schools <- read_schools("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
                                                                             pums = "pums", 
                                                                             schools = "schools/2013", 
                                                                             lookup = "tables", 
                                                                             shapefiles = "tiger", 
-                                                                            workplaces = "workplaces")) 
+                                                                            workplaces = "workplaces",
+                                                                            marginals = "marginals")) 
 
-
+# Workplaces -----
 sd_workplaces <- read_workplaces("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
                                                                                   pums = "pums", 
                                                                                   schools = "schools", 
                                                                                   lookup = "tables", 
                                                                                   shapefiles = "tiger", 
-                                                                                  workplaces = "workplaces"))
+                                                                                  workplaces = "workplaces",
+                                                                                  marginals = "marginals"))
+
+# Marginals ------
+sd_marginals <- read_marginals("data-raw/46", data_group = "US", folders = list(pop_table = "popTables", 
+                                                                                pums = "pums", 
+                                                                                schools = "schools", 
+                                                                                lookup = "tables", 
+                                                                                shapefiles = "tiger", 
+                                                                                workplaces = "workplaces",
+                                                                                marginals = "marginals"))
 
 # Use the Read Data function to get the desired list, and save 
 # this as an .rdata file in the /data folder for use in debugging 
@@ -74,9 +83,11 @@ sd_data <- read_data("data-raw/46", data_group = "US", folders = list(pop_table 
                                                                       schools = "schools/2013", 
                                                                       lookup = "tables", 
                                                                       shapefiles = "tiger", 
-                                                                      workplaces = "workplaces"))
-
+                                                                      workplaces = "workplaces", 
+                                                                      marginals = "marginals"))
 sd_data <- format_data(data_list = sd_data, data_group = "US")
+
+# Uncomment and run to update the sd_data path 
 # devtools::use_data(sd_data, overwrite = TRUE)
 
 
@@ -95,7 +106,7 @@ make_data(pop_table = sd_data$pop_table[1:5, ],
           schools = sd_data$schools, 
           workplaces = sd_data$workplaces, 
           parallel = TRUE, 
-          output_dir = "/home/lee/south_dakota/", 
-          sampling_type = "uniform", 
+          output_dir = "/home/lee/south_dakota/",
+          sampling_method = "uniform", 
+          locations_method = "uniform",  
           convert_count = FALSE)
-
