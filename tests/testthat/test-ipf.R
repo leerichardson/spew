07ea33d7c537571d8b108a5998e2582767c56325
leerichardson <leerichardson2013@gmail.com>
2016-08-27 22:44:44 -0400

    Updating the ipf functions to fix a few bugs for Louisiana

--- tests/testthat/test-ipf.R
+++ tests/testthat/test-ipf.R
@@ -6,10 +6,11 @@ test_that("Iterative Proportional Fitting", {
   library(mipfp)
   
   # Households --------------- 
-  index <- 1
+  index <- 3
   row <- sd_data$pop_table[index, ]
+  place_id <- row[, "place_id"]
   
-  ipf_households <- sample_households(method = "ipf", 
+  ipf_households <- sample_households(method = "uniform", 
                                       n_house = row[, "n_house"], 
                                       pums_h = sd_data$pums$pums_h, 
                                       pums_p = sd_data$pums$pums_p, 
@@ -17,5 +18,11 @@ test_that("Iterative Proportional Fitting", {
                                       place_id = row[, "place_id"],
                                       puma_id = row[, "puma_id"])
   
+  srs_hh_inds <- sample(x = 1:nrow(sd_data$pums$pums_h), size = row[, "n_house"], replace = TRUE)
+  srs_households <- sd_data$pums$pums_h[srs_hh_inds, ]
+  
+  table(srs_households$NP)
+  table(ipf_households$NP)
+  sd_data$marginals$NP$df[sd_data$marginals$NP$df$place_id == place_id, ]
 })
 
