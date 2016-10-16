2758e8a0692df6a972f3b295d1014841831c7573
leerichardson <leerichardson2013@gmail.com>
2016-10-16 15:24:39 -0400

    Updating read function for Jamaica

--- olympus/logfiles/check_logfiles.R
+++ olympus/logfiles/check_logfiles.R
@@ -6,7 +6,7 @@ all_logfiles <- list.files("/mnt/beegfs1/data/shared_group_data/syneco/olympus/l
 							full.names = TRUE)
 
 # Extract the country name from the log-file...
-logfile_names <- unlist(lapply(strsplit(x = all_logfiles, "\\."), function(x) x[3]))
+logfile_names <- unlist(lapply(strsplit(all_logfiles, "\\."), function(x) x[5]))
 
 # Check the logfiles to see which locations executed correctly 
 count <- 0
@@ -18,12 +18,11 @@ for (log in seq_along(all_logfiles)) {
   logfile_path <- all_logfiles[log]
   current_logfile <- readLines(logfile_path)
   current_name <- logfile_names[log]
-  
+ 
   # Print out the results of the check function 
-  result_msg <- paste0(current_name, ": ", check_logfile(current_logfile))
-  print(result_msg)
+  result <- spew:::check_logfile(current_logfile)
   
-  if (check_logfile(current_logfile) == "Complete!") {
+  if (result == "Complete!") {
     count <- count + 1
     complete_locations <- c(complete_locations, current_name)
   } else {
---
+++ olympus/spew_interactive.R
@@ -0,0 +1,114 @@
+options(error = recover)
+
+# ---------- Set the data-group and filepath ------------------------------
+data_group <- "ipums"
+spew_dir <- "/mnt/beegfs1/data/shared_group_data/syneco/spew_1.2.0"
+region <- "asia"
+subregion <- "eastern_asia"
+iso3 <- "chn"
+base_dir <- file.path(spew_dir, region, subregion, iso3)
+# -------------------------------------------------------------------------
+
+# Set the library paths so it checks the personal library first 
+# (Used mainly to get the data.table development version)
+personal_lib <- grep("leerich", .libPaths())
+.libPaths(new = c(.libPaths()[personal_lib]))
+
+# Load SPEW and methods package
+library(spew)
+library(methods) 
+
+# Spatial packages 
+library(sp)
+library(maptools)
+library(rgeos)
+
+# Speed packages 
+library(doParallel)
+library(foreach)
+library(data.table)
+library(bit64)
+library(mipfp)
+
+# Data manipulation packages 
+library(stringdist)
+library(plyr)
+
+# Set the folders and other specific inputs ---------------
+if (data_group == "US") {
+  vars <- list(household = c("RT", "TYPE", "SERIALNO", "PUMA", "REGION", "ST", "HINCP", "NP"), 
+                  person = c("RT", "SERIALNO", "PUMA", "ST", "SEX", "AGEP",  "SCH", "SCHG", "RELP", 
+                             "HISP", "ESR", "PINCP", "NATIVITY", "OCCP", "POBP", "RAC1P"))
+  folders <- list(pop_table = "counts/natstat/2010/tract", 
+		           pums = "pums/natstat/2013/puma", 
+		           shapefiles = "shapefiles/natstat/2010/tract", 
+		           roads = "roads/natstat/2010/county", 
+		           schools = "schools/natstat/2013/county", 
+		           lookup = "lookup/natstat/2010/tract", 
+		           workplaces = "workplaces/natstat/2009/county", 
+		           marginals = "marginals/natstat/2014/tract")
+	sampling_method <- "ipf"
+	locations_method <- "roads"
+	parallel <- TRUE
+	convert_count <- FALSE
+
+	# No schools/workplaces for Puerto Rico
+	if (base_dir == "/mnt/beegfs1/data/shared_group_data/syneco/spew_1.2.0/americas/northern_america/usa/72") {
+	  folders <- list(pop_table = "counts/natstat/2010/tract", 
+                       pums = "pums/natstat/2013/puma", 
+                       shapefiles = "shapefiles/natstat/2010/tract", 
+                       roads = "roads/natstat/2010/county", 
+                       lookup = "lookup/natstat/2010/tract", 
+                       marginals = "marginals/natstat/2014/tract")
+	}
+
+} else if (data_group == "ipums") {	
+	print("Ipums!")
+	folders <- list(pop_table = "counts", 
+	                  pums = "pums", 
+	                  shapefiles = "shapefiles")
+	vars = list(household = c("COUNTRY","YEAR","SAMPLE","SERIAL","PERSONS","HHWT",
+								"FORMTYPE","REGIONW","GEOLEV1","GEOLEV2","HHTYPE",
+								"PERNUM","PERWT","RELATE","RELATED"), 
+				person = c("SERIAL","AGE","SEX","RACE","SCHOOL","INCTOT"))
+	sampling_method <- "uniform"
+	locations_method <- "uniform"
+	parallel <- TRUE
+	convert_count <- TRUE
+
+} else if (data_group == "none") {
+	# Set the custom file-paths for Canada!
+	if (basename(base_dir) == "can") {
+		folders <- list(pop_table = file.path(base_dir, "input/counts/natstat/2011/4/pop_table.csv"), 
+                 pums = list(pums_h = file.path(base_dir, "input/pums/natstat/2011/4/pums_h.csv"),  
+                 			 pums_p = file.path(base_dir, "input/pums/natstat/2011/4/pums_p.csv")),
+                 shapefiles = file.path(base_dir, "input/shapefiles/natstat/2011/4/canada_shapefiles.shp"))
+
+		# Set the specific variables for Canada 
+		vars = list(household = c("SERIALNO", "puma_id"), 
+					person = c("SERIALNO", "AGEGRP","HRSWRK","IMMSTAT",
+						"INCTAX","MODE","OCC","POB","RELIGION","SEX"))
+			sampling_method <- "uniform"
+		locations_method <- "uniform"
+		parallel <- TRUE
+		convert_count <- TRUE
+	}
+}
+
+# Print out the parameters of this call to SPEW for the log-file 
+print(paste0("Spew Version: ", packageVersion("spew")))
+print(paste0("Directory: ", base_dir))
+print(paste0("Data Group: ", data_group))
+print(paste0("Parallel: ", parallel))
+print(paste0("Sampling People Method: ", sampling_method))
+print(paste0("Sampling Locations Method: ", locations_method))
+
+# Call the SPEW wrapper function 
+call_spew(base_dir = base_dir, 
+	        folders = folders, 
+	        data_group = data_group, 
+	        parallel = parallel, 
+	        sampling_method = sampling_method, 
+	        locations_method = locations_method, 
+	        convert_count = convert_count, 
+	        vars = vars)
