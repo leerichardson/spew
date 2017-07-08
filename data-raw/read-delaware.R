1ea02ad2972d05143635bd76143ade018ede8cd8
leerichardson <leerichardson2013@gmail.com>
2017-07-08 13:32:59 -0400

    Updated delaware tests and data

---
+++ data-raw/read-delaware.R
@@ -0,0 +1,22 @@
+# Lee Richardson 
+# July 7, 2013
+# Purpose: Read delaware data from spew_1.2.0 directory structure 
+# using the read.R functions, and save this into data/
+
+# Get filepath for delaware input directory
+delaware_path <- system.file("extdata/10", package = "spew")
+
+# Set filepaths of subdirectories with each input data type 
+delaware_folders <- list(pop_table = "counts/natstat/2010/tract", 
+                         pums = "pums/natstat/2013/puma", 
+                         shapefiles = "shapefiles/natstat/2010/tract", 
+                         roads = "roads/natstat/2010/county", 
+                         schools = "schools/natstat/2013/county", 
+                         lookup = "lookup/natstat/2010/tract", 
+                         workplaces = "workplaces/natstat/2009/county", 
+                         marginals = "marginals/natstat/2014/tract")
+
+# Call spews read_data function on the delaware data-directory 
+delaware <- read_data(base_dir = delaware_path, folders = delaware_folders, data_group = "US")
+
+devtools::use_data(delaware)
--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -6,8 +6,8 @@ test_that("United States functions", {
   
   # Make sure we are using the correct data-raw directory 
   # as opposed to the test/testthat one within the package 
-  spew_dir <- system.file("", package = "spew")
-  data_path <- paste0(spew_dir, "/", "data-raw/10/input")
+  data_path <- system.file("extdata/10/input", package = "spew")
+  
   
   # Skip if the delaware data is not available!  
   if (!file.exists(data_path)) {
@@ -24,40 +24,40 @@ test_that("United States functions", {
                            workplaces = "workplaces/natstat/2009/county", 
                            marginals = "marginals/natstat/2014/tract")
 
-  # Pop Table --------------------------------
+  # Pop Table ---
   delaware_poptable <- read_pop_table(data_path, 
                                       data_group = "US", 
                                       folders = delaware_folders)
   
-  ## Data frame with the correct dimensions 
+  # Data-frame has correct dimensions 
   expect_equal(nrow(delaware_poptable), 218)
   expect_equal(ncol(delaware_poptable), 4)
   expect_equal(class(delaware_poptable), "data.frame")
   
-  ## Making sure stringAsFactors=FALSE 
+  # Make sure stringAsFactors=FALSE 
   expect_equal(any(lapply(delaware_poptable, class) == "factor"), FALSE)
   
-  ## Check the Standardization Function
+  # Check the Standardization Function
   standard_poptable <- standardize_pop_table(pop_table = delaware_poptable, data_group = "US")
   expect_equal("place_id" %in% names(standard_poptable), TRUE)
   
-  ## PUMS -------------------------------
+  # PUMS ---
   delaware_pums <- read_pums(data_path, 
                              data_group = "US", 
                              folders = delaware_folders, 
                              vars = list(household = NA, person = NA))
   
-  ## Test the rows of households are larger 
+  # Test the rows of households are larger 
   expect_true(names(delaware_pums)[1] == "pums_h")
   expect_true(nrow(delaware_pums$pums_p) > nrow(delaware_pums$pums_h))
   expect_true(nrow(delaware_pums$pums_p) == 8767)
   expect_true(nrow(delaware_pums$pums_h) == 4449)
   
-  ## Verify every column is either an integer or a character   
+  # Verify every column is either an integer or a character   
   col_classes <- unlist(c(lapply(delaware_pums$pums_h, class), lapply(delaware_pums$pums_p, class)))
   expect_equal(any(!(col_classes %in% c("character", "integer"))), FALSE)
 
-  ## Verify that the standardized PUMS has the appropriate columns   
+  # Verify that the standardized PUMS has the appropriate columns   
   standard_pums <- standardize_pums(delaware_pums, data_group = "US")  
   expect_true("puma_id" %in% names(standard_pums$pums_h))
   expect_true("puma_id" %in% names(standard_pums$pums_p))    
@@ -70,11 +70,11 @@ test_that("United States functions", {
                              vars = list(household = c("SERIALNO", "ST"), 
                                          person = c("SERIALNO", "ST")))
   
-  expect_equal(ncol(delaware_pums$pums_h), 2)  
+  expect_equal(ncol(delaware_pums$pums_h), 2) 
   expect_equal(names(delaware_pums$pums_h), c("SERIALNO", "ST"))
   expect_equal(ncol(delaware_pums$pums_h), 2)
   
-  # Shapefile --------------------------
+  # Shapefile ---
   delaware_shape <- read_shapefiles(data_path, 
                                     data_group = "US", 
                                     folders = delaware_folders)
@@ -82,16 +82,16 @@ test_that("United States functions", {
   expect_true(class(delaware_shape$roads) == "character")
   
   # Test the reading roads functions, look for speedups   
-  roads_path <- paste0(spew_dir, "/data-raw/46/tiger/roads_46")
-  sd_roads <- read_roads(path_to_roads = roads_path, road_id = "46111")
-  expect_true(class(sd_roads) == "SpatialLinesDataFrame")
+  roads_path <- paste0(data_path, "/", delaware_folders$roads)
+  delaware_roads <- read_roads(path_to_roads = roads_path, road_id = "10001")
+  expect_true(class(delaware_roads) == "SpatialLinesDataFrame")
   
   # Test the standardization functions
   standard_shape <- standardize_shapefiles(delaware_shape, data_group = "US")
   expect_true(length(standard_shape$shapefile$place_id) == 218)
   expect_true(class(standard_shape$shapefile$place_id) == "character")
   
-  # Schools ----------------------------
+  # Schools ---
   delaware_schools <- read_schools(data_path, 
                                    data_group = "US", 
                                    folders = delaware_folders)
@@ -101,13 +101,13 @@ test_that("United States functions", {
   expect_equal(class(delaware_schools$public$StNo), "character")
   expect_equal(class(delaware_schools$public$CoNo), "character")
   
-  # Workplaces --------------------------
+  # Workplaces ---
   delaware_workplaces <- read_workplaces(data_path, 
                                          data_group = "US", 
                                          folders = delaware_folders)
   expect_true(class(delaware_workplaces) == "data.frame")
   
-  # Marginals ---------------------------
+  # Marginals ---
   delaware_marginals <- read_marginals(data_path, 
                                        data_group = "US", 
                                        folders = delaware_folders)
@@ -116,14 +116,15 @@ test_that("United States functions", {
   expect_true(class(delaware_marginals[[1]]) == "list")
   expect_true(class(delaware_marginals[[1]][[1]]) == "data.frame")
   
-  # Overall --------------------------------
-  base_path <- file.path(spew_dir, "data-raw/10")
-  delaware <- read_data(base_dir = base_path, folders = delaware_folders, data_group = "US", time = FALSE)
+  # Overall ---
+  base_path <- system.file("extdata/10", package = "spew")
+  delaware <- read_data(base_dir = base_path, 
+                        folders = delaware_folders, 
+                        data_group = "US")
+  
   expect_true(all.equal(names(delaware), c("pop_table", "pums", "lookup", "shapefiles",
                                            "schools", "workplaces", "marginals", "moments")))
 })
-
-
 
 test_that("ipums functions", {
   # Make sure we are using the correct data-raw directory 
@@ -223,4 +224,3 @@ test_that("custom group read functions", {
   expect_equal(class(pums$pums_h), "data.frame")
   expect_error(standardize_pums(pums, data_group = "none"))
 })
-
