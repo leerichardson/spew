2af7dd3e219390e0baf862c44b743f4a5d90996e
leerichardson <leerichardson2013@gmail.com>
2017-09-14 10:21:02 -0400

    documenting datasets

---
+++ R/extra-docs.R
@@ -0,0 +1,25 @@
+#' spew: an R package for generating synthetic ecosystems
+#'
+#' @docType package
+#' @aliases spew-package
+#' @name spew-package 
+NULL
+
+#' Input data for Keny County, Delaware 
+#' 
+"delaware"
+
+#' Input data for Uruguay
+#' 
+"uruguay"
+
+#' Input data for Tartanville
+#' 
+"tartanville"
+
+#' An example marginal distribution table 
+#' 
+"us_pums_sf"
+
+#' Table of US states and counties 
+"us"
